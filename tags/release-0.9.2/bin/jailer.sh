ea5afa75b13782a7d407dd603e2b1d691d776678
rwisser <rwisser@3dd849cd-670e-4645-a7cd-dd197c8d0e81>
2007-06-13 07:33:53 +0000

    Tagging the 0.9.2 release.

---
+++ tags/release-0.9.2/bin/jailer.sh
@@ -0,0 +1,21 @@
+JDBCLIB=/opt/IBM/db2/V8.1/java
+LIB=lib
+CP=.:out:out/jailer.jar
+
+# JDBC-driver
+CP=$CP:$JDBCLIB/db2jcc.jar
+CP=$CP:$JDBCLIB/db2jcc_license_cu.jar
+CP=$CP:/home/wisser/mysql-connector-java-5.0.6/mysql-connector-java-5.0.6-bin.jar
+
+# configuration files in the config directory
+CP=$CP:config
+
+# the libraries
+CP=$CP:$LIB/junit.jar
+CP=$CP:$LIB/commons-logging.jar
+CP=$CP:$LIB/log4j.jar
+CP=$CP:$LIB/args4j.jar
+CP=$CP:$LIB/spring.jar
+CP=$CP:$LIB/jailer.jar
+
+java -cp $CP org.jailer.Jailer $@
