ef42443c93c44b304640cc7bd489f43f198b443c
rwisser <rwisser@3dd849cd-670e-4645-a7cd-dd197c8d0e81>
2007-07-03 06:51:51 +0000

    Tagging the 0.9.5 release.

---
+++ tags/release-0.9.5/script/ddl-template.sql
@@ -0,0 +1,52 @@
+-- Jailer
+
+DROP TABLE ENTITY;
+DROP TABLE DEPENDENCY;
+DROP TABLE ENTITY_SET_ELEMENT;
+DROP TABLE ENTITY_GRAPH;
+
+CREATE TABLE ENTITY_GRAPH
+(
+   id              INTEGER NOT NULL,
+   age             INTEGER NOT NULL,
+
+   CONSTRAINT j_pk_graph PRIMARY KEY(id)
+);
+
+CREATE TABLE ENTITY
+(
+   r_entitygraph   INTEGER NOT NULL,
+   {0},
+   birthday        INTEGER NOT NULL,
+   type            varchar(50) NOT NULL,
+
+   {1},
+   PRE_TYPE        varchar(50),
+   association     INTEGER,
+
+   CONSTRAINT j_pk_entity PRIMARY KEY(r_entitygraph, {4}),
+   CONSTRAINT j_fk_graph_e FOREIGN KEY (r_entitygraph) REFERENCES entity_graph(id)
+);
+
+CREATE INDEX ix_entity_brthdy ON entity (r_entitygraph, birthday, type);
+
+CREATE TABLE ENTITY_SET_ELEMENT
+(
+   set_id          INTEGER NOT NULL,
+   {0},
+
+   CONSTRAINT j_pk_entityset PRIMARY KEY(set_id, {4})
+);
+
+CREATE TABLE DEPENDENCY
+(
+   r_entitygraph   INTEGER NOT NULL,
+   {2},
+   {3},   
+
+   CONSTRAINT j_fk_graph_d FOREIGN KEY (r_entitygraph) REFERENCES entity_graph(id)
+);
+
+CREATE INDEX ix_dependency_from ON dependency (r_entitygraph, {5});
+CREATE INDEX ix_dependency_to ON dependency (r_entitygraph, {6});
+
---
+++ tags/release-0.9.5/src/org/jailer/util/PrintUtil.java
@@ -0,0 +1,110 @@
+/*
+ * Copyright 2007 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.jailer.util;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.text.MessageFormat;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import org.jailer.datamodel.Table;
+
+
+/**
+ * Utility for printing.
+ * 
+ * @author Wisser
+ */
+public class PrintUtil {
+    
+    /**
+     * Converts a set of tables into a string.
+     * 
+     * @param tables the set
+     */
+    public static String tableSetAsString(Set<Table> tables) {
+        return tableSetAsString(tables, "      ");
+    }
+    
+    /**
+     * Converts a set of tables into a string.
+     * 
+     * @param tables the set
+     */
+    public static String tableSetAsString(Set<Table> tables, String linePrefix) {
+        List<String> tableNames = new ArrayList<String>();
+        for (Table table: tables) {
+            tableNames.add(table.getName());
+        }
+        Collections.sort(tableNames);
+        StringBuffer str = new StringBuffer();
+        str.append(" { ");
+        int i = 1;
+        for (String tableName: tableNames) {
+            if (i > 1) {
+                str.append(", ");
+            }
+            if (i % 5 == 0 && linePrefix != null) {
+                str.append("\n" + linePrefix);
+            }
+            str.append(tableName);
+            ++i;
+        }
+        str.append(" }");
+        return str.toString();
+    }
+
+    /**
+     * Cache for {@link #applyTemplate(String, Object[])}.
+     */
+    private static Map<String, StringBuffer> templateCache = new HashMap<String, StringBuffer>();
+    
+    /**
+     * Applies arguments to template.
+     * 
+     * @param template file name of template
+     * @param arguments the arguments
+     * 
+     * @return template with arguments filled in
+     */
+    public static String applyTemplate(String template, Object[] arguments) throws FileNotFoundException, IOException {
+        StringBuffer sb = templateCache.get(template);
+        if (sb == null) {
+            sb = new StringBuffer();
+            BufferedReader reader = new BufferedReader(new FileReader(template));
+            String line = null;
+            while ((line = reader.readLine()) != null) {
+                sb.append(line + "\n");
+            }
+            reader.close();
+            templateCache.put(template, sb);
+        }
+        
+        return MessageFormat.format(sb.toString(), arguments);
+    }
+
+}
